ed2b26a9fe1723859467a85ee8ec480039093815
Jeremias Granqvist <Jeremias.granqvist@gmail.com>
2025-01-14 12:03:49 +0100

    Add project files.

---
+++ DB_Labb3/Model/BaseClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Labb3.Model
+{
+    public class BaseClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DB_Labb3/View/MainWindowView.cs
@@ -0,0 +1,13 @@
+﻿using DB_Labb3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Labb3.View
+{
+    public class MainWindowView : BaseClass
+    {
+    }
+}
